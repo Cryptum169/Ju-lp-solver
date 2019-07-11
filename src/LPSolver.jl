82ae084e7f7184689f955bd7f748b0f577c5bb92
Moelf <jerryling315@gmail.com>
2019-07-12 01:27:56 +0200

    1% optimization without knowing how things work

--- src/LPSolver.jl
+++ src/LPSolver.jl
@@ -5,22 +5,18 @@ using LinearAlgebra
 export simplex
 @inbounds function simplex(objFunc::Array, A::Array, B::Array, sign::Array)
     num_obj_var = size(objFunc, 2)
+    num_of_constraint = size(A, 2); # -> index from 1
 
     num_bigM = sum(sign .- 1) ÷ -2;
     big_M = num_bigM != 0;
     arg_where = nothing
+    big_M_matrix = zeros(num_of_constraint, num_bigM);
+
 
     if big_M
         arg_where = findall(x -> x < 0, sign)
-    end
-
-    num_of_constraint = size(A, 2); # -> index from 1
-    big_M_value = maximum(abs.(B)) * 1000;
-    big_M_vector = ones(num_bigM, 1) .* big_M_value;
-
-    big_M_matrix = zeros(num_of_constraint, num_bigM);
-
-    if big_M
+        big_M_value = maximum(abs.(B)) * 1000;
+        big_M_vector = ones(num_bigM, 1) .* big_M_value;
         for (idx, value) in enumerate(arg_where)
             big_M_matrix[value[2], idx] = 1
         end
@@ -34,9 +30,7 @@ export simplex
         slack_var_matrix[value[2], value[2]] = -1;
     end
 
-    tableau = hcat(A, slack_var_matrix, big_M_matrix, B');
-
-    tableau = vcat(objFunc, tableau);
+    tableau = vcat(objFunc, hcat(A, slack_var_matrix, big_M_matrix, B'));
 
     if big_M
         for value in arg_where
@@ -52,37 +46,32 @@ export simplex
         min_idx = argmin(tableau[1, 1:end-1])
         result = Tuple{Float64, Int64}[]
         for (idx, items) in enumerate(tableau[2:end, min_idx])
-            if items == 0.
-                continue
-            end
+            items == 0 && continue
             values = tableau[idx + 1, end] / items
-            if values < 0
-                continue
-            end
+            values < 0 && continue
             temp = (values, idx);
             push!(result, (temp));
         end
 
-        data = sort(result, by = x -> x[1]);
-        normalizing_constrant = tableau[data[1][2] + 1, min_idx]
-        tableau[data[1][2] + 1, :] /= abs(normalizing_constrant)
+        sort!(result, by = x -> x[1]);
+        normalizing_constrant = tableau[result[1][2] + 1, min_idx]
+        tableau[result[1][2] + 1, :] /= abs(normalizing_constrant)
         for k = 1:num_of_constraint + 1
-            if k == data[1][2] + 1
+            if k == result[1][2] + 1
                 continue
             end
-
             coefficient = tableau[k, min_idx]
-            tableau[k, :] -= tableau[data[1][2] + 1, :] * coefficient
+            tableau[k, :] -= tableau[result[1][2] + 1, :] * coefficient
         end
     end
 
-    result = Float64[]
+    result = Array{Float64}(undef,num_obj_var)
     for k = 1:num_obj_var
         if tableau[1, k] != 0
-            append!(result, 0);
+            result[k] = 0;
         else
-            item_idx = findall(x -> x == 1, tableau[2:end, k]);
-            append!(result, tableau[item_idx[1] + 1, end]);
+            item_idx = findfirst(x -> x == 1, tableau[2:end, k]);
+            result[k] = tableau[item_idx + 1, end];
         end
     end
 
